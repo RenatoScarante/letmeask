197f6967b715df7a944e7e270b6ce974a2e38d94
Renato Scarante <renatoscarante@MacBook-Pro-de-Renato.local>
2021-08-24 15:52:47 -0300

    NLW Together - Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <Button text='Clique aqui' />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
